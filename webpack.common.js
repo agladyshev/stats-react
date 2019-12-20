d3d133f80ac39344217551cb79c41b157cafb52c
Alexey Gladyshev <aglad@pm.me>
2019-12-20 11:16:28 +0300

    dev: setup webpack for dev and prod

--- webpack.common.js
+++ webpack.common.js
@@ -1,9 +1,7 @@
 const path = require("path");
-const webpack = require("webpack");
 
 module.exports = {
-  entry: "./src/index.js",
-  mode: "development",
+  entry: { app: "./src/index.js" },
   module: {
     rules: [
       {
@@ -23,12 +21,5 @@ module.exports = {
     path: path.resolve(__dirname, "dist/"),
     publicPath: "/dist/",
     filename: "bundle.js"
-  },
-  devServer: {
-    contentBase: path.join(__dirname, "public/"),
-    port: 3000,
-    publicPath: "http://localhost:3000/dist/",
-    hotOnly: true
-  },
-  plugins: [new webpack.HotModuleReplacementPlugin()]
+  }
 };
