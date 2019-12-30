ba27f239fc289fdf2a30b7b6a10a14d2bc9aa20d
Alexey Gladyshev <aglad@pm.me>
2019-12-30 14:26:49 +0300

    feat: Display and style youtube stats

--- src/App.js
+++ src/App.js
@@ -1,9 +1,72 @@
 import React, { Component } from "react";
 import "./App.css";
 
+class Youtube extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      viewsPerVideoRecent: Math.round(
+        props.youtube_views_recent / props.youtube_videos_recent
+      ),
+      likesPerVideo: Math.round(
+        props.youtube_likes_recent / props.youtube_videos_recent
+      ),
+      dislikesPerVideo: Math.round(
+        props.youtube_dislikes_recent / props.youtube_videos_recent
+      ),
+      commentsPerVideo: Math.round(
+        props.youtube_comments_recent / props.youtube_videos_recent
+      )
+    };
+  }
+  render() {
+    return (
+      <details>
+        <summary>
+          <img src="./icons/youtube.png"></img>
+          <h6>youtube</h6>
+        </summary>
+        {/* <p>Overall</p> */}
+        <dl>
+          <div>
+            <dt>Subscribers</dt>
+            <dd>{this.props.youtube_subscribers}</dd>
+          </div>
+          <div>
+            <dt>Videos</dt>
+            <dd>{this.props.youtube_videos}</dd>
+          </div>
+        </dl>
+        <p>Per video</p>
+        <dl>
+          <div>
+            <dt>Views</dt>
+            <dd>{this.state.viewsPerVideoRecent}</dd>
+          </div>
+          <div>
+            <dt>Likes</dt>
+            <dd>{this.state.likesPerVideo}</dd>
+          </div>
+          <div>
+            <dt>Dislikes</dt> <dd>{this.state.dislikesPerVideo}</dd>
+          </div>
+          <div>
+            <dt>Comments</dt> <dd>{this.state.commentsPerVideo}</dd>
+          </div>
+        </dl>
+      </details>
+    );
+  }
+}
+
 class Account extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      youtube: Object.fromEntries(
+        Object.entries(props).filter(([key]) => key.includes("youtube"))
+      )
+    };
   }
   render() {
     const element = (
@@ -14,12 +77,14 @@ class Account extends Component {
             src={this.props.youtube_thumbnail || this.props.twitter_pic}
           ></img>
         </picture>
-        <div className="account-main-stats">
+        <article className="account-description">
           <h2>{this.props.name}</h2>
-          <div>YouTube subscribers: {this.props.youtube_subscribers}</div>
           <div>Twitter followers: {this.props.twitter_followers}</div>
           <div>Telegram subscribers: {this.props.tg_channel_subscribers}</div>
-        </div>
+        </article>
+        <ul className="account-stats">
+          <Youtube {...this.state.youtube} />
+        </ul>
       </li>
     );
     return element;
